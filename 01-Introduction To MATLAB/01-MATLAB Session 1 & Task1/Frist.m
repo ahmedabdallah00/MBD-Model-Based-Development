1fb568d924506a5df9ea0bb4df10e1070a9d7ed0
AHMED ABDULLAH ALGHARIB <ahmed.algharib42@gmail.com>
2025-02-03 05:30:26 -0800

    Initial Commit

---
+++ 01-Introduction To MATLAB/01-MATLAB Session 1 & Task1/Frist.m
@@ -0,0 +1,8 @@
+A=[1 2 3 , 4 5 6, 7 8 9];
+s=length(A);
+C= rand(2,3,4);
+D=[];
+isEmpty= (size(D)==[0 0]);
+matrix=[];
+[rowcount,colcount] = size(matrix);
+disp(['Rows: ',num2str(rowcount) ', Colums: ',num2str(colcount)]);
